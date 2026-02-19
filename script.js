2cd51b0d37e0f5ae909962f18640eba760b1f6dc
LksKrjck <krajciklza@gmail.com>
2026-02-19 08:59:47 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+const odkazy = document.querySelectorAll(".nav-link");
+const basne = document.querySelectorAll(".basen");
+
+// schovanie všetkých básničiek
+
+odkazy.forEach(odkaz => {
+    odkaz.addEventListener("click", (event) => {
+        event.preventDefault();
+
+        const cielId = odkaz.getAttribute("href").substring(1);
+        const ciel = document.getElementById(cielId);
+        
+        basne.forEach(basen => basen.classList.add("schovate"));
+
+        if (ciel) {
+            ciel.classList.remove("schovate");
+            window.scrollTo({ top: 0, behavior: "smooth" });
+        }
+    });
+});
+
+// zobrazenie náhodnej básničky na novej stránke
+
+let randomPoem = Math.floor(Math.random() * basne.length);
+
+basne[randomPoem].classList.remove("schovate");
+})
+
+// nočný režím
+
+const nightModeButton = document.querySelector("#night");
+const wholeSite = document.querySelector("html");
+
+    nightModeButton.addEventListener("click", () => {
+wholeSite.classList.toggle("night")
+
+    })
+
+
+// prehrávanie hudobných ukážok
+
+let currentAudio = null;
+
+const playButtons = document.querySelectorAll(".playback");
+
+playButtons.forEach(button => {button.addEventListener("click", function() {
+
+    let preview = this.dataset.audio;
+
+        if (currentAudio) {
+          if (currentAudio.src.includes(preview)) {
+            currentAudio.pause();
+            currentAudio.currentTime = 0;
+            currentAudio = null;
+            return;
+          } 
+          currentAudio.pause();
+          currentAudio.currentTime = 0;}
+
+          currentAudio = new Audio (preview);
+          currentAudio.play();
+})})
